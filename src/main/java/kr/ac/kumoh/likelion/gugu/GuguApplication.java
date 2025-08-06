5be9379763f8228084ab0b2d410039bec6283c2d
Jiho Park <jiho3877@naver.com>
2025-08-06 15:54:14 +0900

    Add initial project source files

---
+++ src/main/java/kr/ac/kumoh/likelion/gugu/GuguApplication.java
@@ -0,0 +1,13 @@
+package kr.ac.kumoh.likelion.gugu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuguApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuguApplication.class, args);
+	}
+
+}
