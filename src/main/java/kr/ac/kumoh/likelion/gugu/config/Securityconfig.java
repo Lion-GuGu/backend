05cddf99b5f2012304e5650f6e5ede9c1f158126
dokhi777 <parkjane0717@gmail.com>
2025-08-11 14:03:23 +0900

    Create Securityconfig.java

---
+++ src/main/java/kr/ac/kumoh/likelion/gugu/config/Securityconfig.java
@@ -0,0 +1,20 @@
+package kr.ac.kumoh.likelion.gugu.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class Securityconfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().permitAll() // 모든 요청 허용
+                )
+                .csrf(csrf -> csrf.disable()) // CSRF 비활성화(개발용)
+                .formLogin(login -> login.disable()); // 기본 로그인 폼 비활성화
+        return http.build();
+    }
+}
