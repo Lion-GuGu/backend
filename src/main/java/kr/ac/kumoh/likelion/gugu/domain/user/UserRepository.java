e6664dcf9db3d6b81b47f730edad6e302321bbcc
dokhi777 <parkjane0717@gmail.com>
2025-08-12 16:52:16 +0900

    회원가입 기능 구현

--- src/main/java/kr/ac/kumoh/likelion/gugu/domain/user/UserRepository.java
+++ src/main/java/kr/ac/kumoh/likelion/gugu/domain/user/UserRepository.java
@@ -3,4 +3,6 @@ package kr.ac.kumoh.likelion.gugu.domain.user;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+    boolean existsByUsername(String username);
+    boolean existsByEmail(String email);
 }
