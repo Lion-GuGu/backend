d95b9325a330f176aebdcb7ef755c96f00fa22ee
dokhi777 <parkjane0717@gmail.com>
2025-08-24 03:11:12 +0900

    컴포넌트 스캔 범위 수정

--- src/main/java/kr/ac/kumoh/likelion/gugu/GuguApplication.java
+++ src/main/java/kr/ac/kumoh/likelion/gugu/GuguApplication.java
@@ -3,7 +3,7 @@ package kr.ac.kumoh.likelion.gugu;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "kr.ac.kumoh.likelion.gugu")
 public class GuguApplication {
 
 	public static void main(String[] args) {
