0b40ddd0627f86900c96178d30656f253a11ef87
dokhi777 <parkjane0717@gmail.com>
2025-08-24 02:20:04 +0900

    null 값 확인

--- src/main/java/kr/ac/kumoh/likelion/gugu/user/web/UserResponse.java
+++ src/main/java/kr/ac/kumoh/likelion/gugu/user/web/UserResponse.java
@@ -1,5 +1,6 @@
 package kr.ac.kumoh.likelion.gugu.user.web;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -7,6 +8,7 @@ import lombok.Getter;
 @Getter
 @Builder
 @AllArgsConstructor
+@JsonInclude(JsonInclude.Include.ALWAYS)
 public class UserResponse {
     private Long id;
     private String username;
